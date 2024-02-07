5b652e36e94f16d4986ae50901fe00bcf5a5f3ce
Tomás Arrativel <arrativeltomas@gmail.com>
2024-02-07 09:04:15 -0300

    Adding different values at once

---
+++ alter_table_example_4.sql
@@ -0,0 +1,9 @@
+INSERT INTO product VALUES 
+('1', 'Grandview', 'Allen Edmonds', 'Classic shoes for bussines man'),
+('1', 'Clarkston', 'Allen Edmonds', 'Extravagant shoes for bussines man'),
+('2', 'Vanentian Loafer', 'Mezland', 'Sueder upper, leather sole'),
+('2', 'Malek', 'Johnstone & Murphy', 'Casual shoes for casual men'),
+('3', 'Air Force 1', 'Nike', 'Modern shoes for athletic people'),
+('3', 'Revel 3', 'Brooks', 'Style to spare, now even softer');
+
+SELECT * FROM product;

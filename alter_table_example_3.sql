efd5ddd7bd79dd7d223875ca25cce909f7e1a67b
Tomás Arrativel <arrativeltomas@gmail.com>
2024-02-07 08:52:42 -0300

    Inserting into table some values at once and getting the table

---
+++ alter_table_example_3.sql
@@ -0,0 +1,5 @@
+INSERT INTO product_type(name) VALUES('Bussines');
+INSERT INTO product_type(name) VALUES('Casual');
+INSERT INTO product_type(name) VALUES('Athletic');
+
+SELECT * FROM product_type;
